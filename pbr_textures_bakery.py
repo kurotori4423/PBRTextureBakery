9b93f4d2eec18ee804fa261ad9d4f86edd447c27
kurotori4423 <kurotori4423@gmail.com>
2019-07-12 23:36:31 +0900

    マルチマテリアルに対応

--- pbr_textures_bakery.py
+++ pbr_textures_bakery.py
@@ -86,49 +86,27 @@ class PBR_CreateTextrue(bpy.types.Operator):
 
         context.scene.render.engine = 'CYCLES'
 
-        col = self.BakeTexture(context.scene.bakery_filename, ao, 'Base Color', context.scene.bakery_resolution)
+        col = self.BakeTextureMultiMat(context.scene.bakery_filename, ao, 'Base Color', context.scene.bakery_resolution)
         if col is None:
             return{'FINISHED'}
         saveTexture(context, col, 'PNG', col.name, context.scene.bakery_out_directory)
 
-        met = self.BakeTexture(context.scene.bakery_filename, ao, 'Metallic', context.scene.bakery_resolution)
+        met = self.BakeTextureMultiMat(context.scene.bakery_filename, ao, 'Metallic', context.scene.bakery_resolution)
         if met is None:
             return{'FINISHED'}
         saveTexture(context, met, 'PNG', met.name, context.scene.bakery_out_directory)
 
-        nrm = self.BakeNormal(context.scene.bakery_filename, ao, context.scene.bakery_resolution)
+        nrm = self.BakeNormalMultiMat(context.scene.bakery_filename, ao, context.scene.bakery_resolution)
         if nrm is None:
             return{'FINISHED'}
         saveTexture(context, nrm, 'PNG', nrm.name, context.scene.bakery_out_directory)
 
-        rgh = self.BakeTexture(context.scene.bakery_filename, ao, 'Roughness', context.scene.bakery_resolution)
+        rgh = self.BakeTextureMultiMat(context.scene.bakery_filename, ao, 'Roughness', context.scene.bakery_resolution)
         if rgh is None:
             return{'FINISHED'}
         saveTexture(context, rgh, 'PNG', rgh.name, context.scene.bakery_out_directory)
 
         return{'FINISHED'}
-    
-    def BakeNormal(self, filename, activeObject, resolution):
-        mat = activeObject.data.materials[0]
-        nodes = mat.node_tree.nodes
-
-        if nodes.find('Material Output') == -1:
-            self.report({'ERROR'}, 'Material Output is not found')
-            return None
-        
-        img = bpy.data.images.new(filename + mapTypesSuffix['Normal'], width = resolution, height = resolution)
-        node_uv_map = nodes.new(type = 'ShaderNodeUVMap')
-        node_uv_map.uv_map = activeObject.data.uv_layers[0].name
-        texNode = nodes.new(type='ShaderNodeTexImage')
-        texNode.image = img
-        nodes.active = texNode
-        bpy.ops.object.bake(type='NORMAL', margin = 8, normal_space='TANGENT')
-
-        nodes.remove(node_uv_map)
-        nodes.remove(texNode)
-
-        self.report({'INFO'}, img.name + " : Bake Complete!")
-        return img
 
     # 正しいPBRマテリアルかどうかチェック
     def CheckMaterial(self, material):
@@ -158,121 +136,158 @@ class PBR_CreateTextrue(bpy.types.Operator):
     def CheckNextMixNode(self, mixnode):
         result = True
         for node in mixnode.inputs[1].links:
-            print(node.from_node.name)
             if node.from_node.type == 'MIX_SHADER':
                 result = self.CheckNextMixNode(node.from_node)
             elif node.from_node.type == 'BSDF_PRINCIPLED':
                 result = True and result
             else:
                 result = False and result
-                print("find")
         
         for node in mixnode.inputs[2].links:
-            print(node.from_node.name)
             if node.from_node.type == 'MIX_SHADER':
                 result = self.CheckNextMixNode(node.from_node)
             elif node.from_node.type == 'BSDF_PRINCIPLED':
                 result = True and result
             else:
                 result = False and result
-                print("find")
         
         return result
-
-    def BakeTexture(self, filename, activeObject, mapType, resolution):
-        mat = activeObject.data.materials[0]
-        nodes = mat.node_tree.nodes
-        links = mat.node_tree.links
-
-        if nodes.find('Material Output') == -1:
-            self.report({'ERROR'}, 'Material Output is not found')
-            return None
-
-        matOutput = nodes['Material Output']
-
-        # Material Output に接続されたノードを取得する
-        # リストといいつつ大体１つしか見つからない。
-        matOutputNodeList = []
-
-        for node in matOutput.inputs['Surface'].links:
-            matOutputNodeList.append({'From_Node' : node.from_node, 'From_Socket' : node.from_socket})
+    def BakeNormalMultiMat(self, filename, activeObject, resolution):
+        img = bpy.data.images.new(filename + mapTypesSuffix['Normal'], width = resolution, height = resolution)
         
-        renderNodes = [] # {Emis , BSDF}
+        NodeUVMapList = []
+        NodeTextureList = []
 
-        # プリンシプルBSDFのリスト
-        pBSDFs = []
+        for mat in activeObject.data.materials:
+            nodes = mat.node_tree.nodes
+            node_uv_map = nodes.new(type = 'ShaderNodeUVMap')
+            node_uv_map.uv_map = activeObject.data.uv_layers[0].name
+            texNode = nodes.new(type='ShaderNodeTexImage')
+            texNode.image = img
+            nodes.active = texNode
 
-        for node in nodes:
-            if node.type == 'BSDF_PRINCIPLED':
-                renderNodes.append({ 'Emis' : nodes.new(type ='ShaderNodeEmission'), 'BSDF' : node})
-                pBSDFs.append(node)
-
-        if len(pBSDFs) == 0:
-            self.report({'ERROR'}, 'Principled BSDF Node is not found')
-            return None
-
-        # リンク切断
-        BSDFInConnects = [] # [プリンシプルBSDFノード, BSDFの任意のソケット, BSDFの任意の入力に接続するノード, BSDFの任意の入力に接続するノードのソケット]
-        BSDFOutConnects = [] # [BSDFの出力に接続するソケット、BSDF]
-        for bsdf in pBSDFs:
-            if bsdf.outputs['BSDF'].is_linked:
-                for node in bsdf.outputs['BSDF'].links:
-                    BSDFOutConnects.append({'To_Socket' : node.to_socket, 'BSDF' : bsdf})
-            else:
-                BSDFOutConnects.append({'To_Socket' : None , 'BSDF' : bsdf})
-
-            if bsdf.inputs[mapType].is_linked:
-                for node in bsdf.inputs[mapType].links:
-                    BSDFInConnects.append({ 'BSDF' : bsdf, 'BSDF_Input' : bsdf.inputs[mapType], 'From_Node' : node.from_node, 'From_Socket' : node.from_socket})
-                    links.remove(node)
-            else:
-                BSDFInConnects.append({'BSDF' : bsdf, 'BSDF_Input' : bsdf.inputs[mapType], 'From_Node' : None, 'From_Socket' : None})
+            NodeUVMapList.append(node_uv_map)
+            NodeTextureList.append(texNode)
         
-        # エミッシブノードにリンク結合
-        for i , bsdfIn in enumerate(BSDFInConnects):
-            if bsdfIn['From_Socket'] is None:
-                if mapType == 'Metallic' or mapType == 'Roughness':
-                    renderNodes[i]['Emis'].inputs['Strength'].default_value = BSDFOutConnects[i]['BSDF'].inputs[mapType].default_value
-                elif mapType == 'Base Color':
-                    renderNodes[i]['Emis'].inputs['Color'].default_value = BSDFOutConnects[i]['BSDF'].inputs[mapType].default_value
-                else:
-                    renderNodes[i]['Emis'].inputs['Color'].default_value = [0.5, 0.5, 1.0]
-                if not BSDFOutConnects[i]['To_Socket'] is None:
-                    links.new(renderNodes[i]['Emis'].outputs['Emission'], BSDFOutConnects[i]['To_Socket'])
-            else:
-                links.new(renderNodes[i]['Emis'].inputs['Color']    , bsdfIn['From_Socket']) #エミッシブノードの入力
-                if not BSDFOutConnects[i]['To_Socket'] is None:
-                    links.new(renderNodes[i]['Emis'].outputs['Emission'], BSDFOutConnects[i]['To_Socket'])
-        
-        # ベイク
-        img = bpy.data.images.new(filename + mapTypesSuffix[mapType], width = resolution, height = resolution)
-        node_uv_map = nodes.new(type = 'ShaderNodeUVMap')
-        node_uv_map.uv_map = activeObject.data.uv_layers[0].name
-        texNode = nodes.new(type='ShaderNodeTexImage')
-        texNode.image = img
-        nodes.active = texNode
-        bpy.ops.object.bake(type='EMIT', margin = 8)
+        bpy.ops.object.bake(type='NORMAL', margin = 8, normal_space='TANGENT')
 
-        nodes.remove(node_uv_map)
-        nodes.remove(texNode)
+        for i, mat in enumerate(activeObject.data.materials):
+            nodes = mat.node_tree.nodes
+            nodes.remove(NodeUVMapList[i])
+            nodes.remove(NodeTextureList[i])
 
-        # 再リンク
-        for i , bsdfOut in enumerate(BSDFOutConnects):
-            if not bsdfOut['To_Socket'] is None:
-                links.new(bsdfOut['To_Socket'], bsdfOut['BSDF'].outputs['BSDF'])
-        
-        for i , renderNode in enumerate(renderNodes):
-            nodes.remove(renderNode['Emis'])
-        
-        for bsdfIn in BSDFInConnects:
-            if not bsdfIn['From_Socket'] is None:
-                links.new(bsdfIn['BSDF_Input'], bsdfIn['From_Socket'])
         self.report({'INFO'}, img.name + " : Bake Complete!")
         return img
 
+    def BakeTextureMultiMat(self, filename, activeObject, mapType, resolution):
+        mat = activeObject.data.materials[0]
+
+        renderNodesList = []
+        BSDFInConnectsList = []
+        BSDFOutConnectsList = []
+        NodeUVMapList = []
+        NodeTextureList = []
+
+         # ベイク
+        img = bpy.data.images.new(filename + mapTypesSuffix[mapType], width = resolution, height = resolution)
+
+        for mat in activeObject.data.materials:
+            nodes = mat.node_tree.nodes
+            links = mat.node_tree.links
+
+            matOutput = nodes['Material Output']
+
+            # Material Output に接続されたノードを取得する
+            # リストといいつつ大体１つしか見つからない。
+            matOutputNodeList = []
+
+            for node in matOutput.inputs['Surface'].links:
+                matOutputNodeList.append({'From_Node' : node.from_node, 'From_Socket' : node.from_socket})
+        
+            renderNodes = [] # {Emis , BSDF}
+
+            # プリンシプルBSDFのリスト
+            pBSDFs = []
+
+            for node in nodes:
+                if node.type == 'BSDF_PRINCIPLED':
+                    renderNodes.append({ 'Emis' : nodes.new(type ='ShaderNodeEmission'), 'BSDF' : node})
+                    pBSDFs.append(node)
+
+            if len(pBSDFs) == 0:
+                self.report({'ERROR'}, 'Principled BSDF Node is not found')
+                return None
+
+            # リンク切断
+            BSDFInConnects = [] # [プリンシプルBSDFノード, BSDFの任意のソケット, BSDFの任意の入力に接続するノード, BSDFの任意の入力に接続するノードのソケット]
+            BSDFOutConnects = [] # [BSDFの出力に接続するソケット、BSDF]
+            for bsdf in pBSDFs:
+                if bsdf.outputs['BSDF'].is_linked:
+                    for node in bsdf.outputs['BSDF'].links:
+                        BSDFOutConnects.append({'To_Socket' : node.to_socket, 'BSDF' : bsdf})
+                else:
+                    BSDFOutConnects.append({'To_Socket' : None , 'BSDF' : bsdf})
+
+                if bsdf.inputs[mapType].is_linked:
+                    for node in bsdf.inputs[mapType].links:
+                        BSDFInConnects.append({ 'BSDF' : bsdf, 'BSDF_Input' : bsdf.inputs[mapType], 'From_Node' : node.from_node, 'From_Socket' : node.from_socket})
+                        links.remove(node)
+                else:
+                    BSDFInConnects.append({'BSDF' : bsdf, 'BSDF_Input' : bsdf.inputs[mapType], 'From_Node' : None, 'From_Socket' : None})
+        
+            # エミッシブノードにリンク結合
+            for i , bsdfIn in enumerate(BSDFInConnects):
+                if bsdfIn['From_Socket'] is None:
+                    if mapType == 'Metallic' or mapType == 'Roughness':
+                        renderNodes[i]['Emis'].inputs['Strength'].default_value = BSDFOutConnects[i]['BSDF'].inputs[mapType].default_value
+                    elif mapType == 'Base Color':
+                        renderNodes[i]['Emis'].inputs['Color'].default_value = BSDFOutConnects[i]['BSDF'].inputs[mapType].default_value
+                    else:
+                        renderNodes[i]['Emis'].inputs['Color'].default_value = [0.5, 0.5, 1.0]
+                    if not BSDFOutConnects[i]['To_Socket'] is None:
+                        links.new(renderNodes[i]['Emis'].outputs['Emission'], BSDFOutConnects[i]['To_Socket'])
+                else:
+                    links.new(renderNodes[i]['Emis'].inputs['Color']    , bsdfIn['From_Socket']) #エミッシブノードの入力
+                    if not BSDFOutConnects[i]['To_Socket'] is None:
+                        links.new(renderNodes[i]['Emis'].outputs['Emission'], BSDFOutConnects[i]['To_Socket'])
+            
+            renderNodesList.append(renderNodes)
+            BSDFInConnectsList.append(BSDFInConnects)
+            BSDFOutConnectsList.append(BSDFOutConnects)
+       
+            node_uv_map = nodes.new(type = 'ShaderNodeUVMap')
+            node_uv_map.uv_map = activeObject.data.uv_layers[0].name
+            texNode = nodes.new(type='ShaderNodeTexImage')
+            texNode.image = img
+            nodes.active = texNode
+
+            NodeUVMapList.append(node_uv_map)
+            NodeTextureList.append(texNode)
+
+        bpy.ops.object.bake(type='EMIT', margin = 8)
 
 
+        for index, mat in enumerate(activeObject.data.materials):
+            nodes = mat.node_tree.nodes
+            links = mat.node_tree.links
 
+            nodes.remove(NodeUVMapList[index])
+            nodes.remove(NodeTextureList[index])
+
+            # 再リンク
+            for i , bsdfOut in enumerate(BSDFOutConnectsList[index]):
+                if not bsdfOut['To_Socket'] is None:
+                    links.new(bsdfOut['To_Socket'], bsdfOut['BSDF'].outputs['BSDF'])
+            
+            for i , renderNode in enumerate(renderNodesList[index]):
+                nodes.remove(renderNode['Emis'])
+            
+            for bsdfIn in BSDFInConnectsList[index]:
+                if not bsdfIn['From_Socket'] is None:
+                    links.new(bsdfIn['BSDF_Input'], bsdfIn['From_Socket'])
+        
+        self.report({'INFO'}, img.name + " : Bake Complete!")
+        return img
 
 def register():
     bpy.utils.register_class(PBR_CreateTextrue)
